68ff11414884edf9b9a61f872169c9a7ce3d7c92
James Hale <james@jameshale.net>
2020-09-06 09:35:40 -0400

    Add awscli and docker-compose to install

--- ec2.tf
+++ ec2.tf
@@ -38,11 +38,11 @@ resource "aws_instance" "primary" {
   user_data = <<EOF
 #!/bin/bash
 apt-get update
-apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common awscli
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io
+apt-get install docker-ce docker-ce-cli containerd.io docker-compose
 EOF
 
   tags = {
